d04037302e7e38f9edc2c513e7099414d7730627
Prof Bill <williamt@williamt.com>
2018-03-01 21:51:57 +0000

    Super/Session page and many nits

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,4 +3,10 @@ class Session < ApplicationRecord
   has_many :session_events , dependent: :destroy
   has_many :session_notes
 
+  # returns school ID for a session; it's defined be the session student
+  def school_id
+    student = Student.find( self.session_student)
+    student.school_id
+  end
+
 end
